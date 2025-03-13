f0945c50e3858222b83c885f63c0a13fb5e56aee
Amdebarian-git <flavien.guillonfr@gmail.com>
2025-03-13 19:36:30 +0100

    First Commit

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -1,19 +1,23 @@
 import { Card, List } from "../models/associations.js";
-import Joi from 'joi';
+import Joi from "joi";
 
 const cardController = {
     async index(req, res) {
-        res.status(200).json(await Card.findAll({
-            include: ["list", "tags"],
-            order: [
-                ["position", "ASC"],
-                ["created_at", "DESC"],
-            ],
-        }));
+        res.status(200).json(
+            await Card.findAll({
+                include: ["list", "tags"],
+                order: [
+                    ["position", "ASC"],
+                    ["created_at", "DESC"],
+                ],
+            })
+        );
     },
 
     async show(req, res, next) {
-        const result = await Card.findByPk(req.params.id, { include: ["list", "tags"] });
+        const result = await Card.findByPk(req.params.id, {
+            include: ["list", "tags"],
+        });
 
         if (!result) {
             return next();
@@ -39,12 +43,13 @@ const cardController = {
             return next(error);
         }
 
-        const card = await Card.findByPk(req.params.id, { include: ["list", "tags"] });
+        const card = await Card.findByPk(req.params.id, {
+            include: ["list", "tags"],
+        });
         if (!card) {
             return next();
         }
-
-        // Au lieu de faire un if pour chaque paramètre (ce qui peu faire pas mal de lignes), on peut faire une boucle
+        
         // pour mettre à jour les valeurs qui existent dans req.body.
         for (const key in req.body) {
             // On vérifie que la clé existe avant de mettre à jour => permet de ne pas utilser d'éventuelles
@@ -89,33 +94,44 @@ const cardController = {
         let schema = Joi.object({
             content: Joi.string().min(3).messages({
                 "string.base": "Le contenu doit être une chaîne de caractères",
-                "string.min": "Le contenu doit contenir au moins {#limit} caractères",
+                "string.min":
+                    "Le contenu doit contenir au moins {#limit} caractères",
             }),
             position: Joi.number().integer().greater(0).messages({
                 "number.base": "La position doit être un nombre",
                 "number.integer": "La position doit être un nombre entier",
                 "number.greater": "La position doit être supérieure à {#limit}",
             }),
-            color: Joi.string().regex(/^#([0-9a-fA-F]{3}){1,2}$/).messages({
-                "string.pattern.base": "La couleur doit être un code hexadécimal valide",
-            }),
+            color: Joi.string()
+                .regex(/^#([0-9a-fA-F]{3}){1,2}$/)
+                .messages({
+                    "string.pattern.base":
+                        "La couleur doit être un code hexadécimal valide",
+                }),
             list_id: Joi.number().integer().greater(0).messages({
                 "number.base": "L'identifiant de la liste doit être un nombre",
-                "number.integer": "L'identifiant de la liste doit être un nombre entier",
-                "number.greater": "L'identifiant de la liste doit être supérieur à {#limit}",
+                "number.integer":
+                    "L'identifiant de la liste doit être un nombre entier",
+                "number.greater":
+                    "L'identifiant de la liste doit être supérieur à {#limit}",
             }),
         });
 
         if (req.method === "POST") {
-            schema = schema.fork(["content", "list_id"], field => field.required().messages({
-                "any.required": "Le champ {#label} est requis",
-            }));
+            schema = schema.fork(["content", "list_id"], field =>
+                field.required().messages({
+                    "any.required": "Le champ {#label} est requis",
+                })
+            );
         }
 
         const error = schema.validate(req.body, { abortEarly: false }).error;
 
         return error
-            ? { statusCode: 400, message: error.details.map(detail => detail.message) }
+            ? {
+                  statusCode: 400,
+                  message: error.details.map(detail => detail.message),
+              }
             : null;
     },
 };
